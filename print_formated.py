2cb579c56027e72b052aa4d25c27c280879d2c3e
AmigosKazz <kaznarahandrinarivo@gmail.com>
2024-02-16 06:21:54 +0300

    I have used Python's built-in string formatting to convert the number into different formats (decimal, octal, hexadecimal, and binary).

--- print_formated.py
+++ print_formated.py
@@ -1,12 +1,7 @@
 def print_formatted(number):
-    space = len(bin(number)[2::])
-    for i in range(1,number+1):
-        dec = str(i)
-        octal = oct(i)[2::]
-        hexa = hex(i)[2::]
-        if hexa.isalpha:hexa = hexa.upper()
-        binaire = bin(i)[2::]
-        print(" "*(space-len(dec))+dec, " "*(space+1-len(octal))+octal, " "*(space+1-len(hexa))+hexa," "*(space+1-len(binaire))+binaire, sep="" )
+    width = len(bin(number)[2:])
+    for i in range(1, number + 1):
+        print(f'{i:{width}d} {i:{width}o} {i:{width}X} {i:{width}b}')
 
 number = 17
 print_formatted(number)
